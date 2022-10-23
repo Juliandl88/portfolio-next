7d1b9ef4611053c0eb2e6dd957ddcf912cb4f669
Julian <julian_delisio@hotmail.com>
2022-10-22 22:20:38 -0300

    terminando secciones

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68ebb74701465af5"; }
+/******/ 		__webpack_require__.h = function() { return "1562535907803188"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
